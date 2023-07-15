895c636bb194d43f9b05b20d697be4333ee908f4
Mohammadreza Amini <45162458+mrezaamini@users.noreply.github.com>
2023-07-15 15:43:20 +0330

    Update Header

--- slack-project/src/Header.js
+++ slack-project/src/Header.js
@@ -1,10 +1,30 @@
 import React from "react";
 import styled from "styled-components";
+import { Avatar } from "@mui/material";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import SearchIcon from "@mui/icons-material/Search";
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
 
 function Header() {
   return (
     <HeaderContainer>
-      <h1>HI</h1>
+      {/* left */}
+      <LeftSide>
+        <HeaderAvatar />
+        <AccessTimeIcon />
+      </LeftSide>
+      {/* middle */}
+
+      <Middle>
+        <SearchIcon />
+        <input placeholder="Search..." />
+      </Middle>
+
+      {/* right */}
+
+      <RightSide>
+        <HelpOutlineIcon />
+      </RightSide>
     </HeaderContainer>
   );
 }
@@ -12,7 +32,64 @@ function Header() {
 export default Header;
 
 const HeaderContainer = styled.div`
-  > h1 {
-    color: "red";
+  display: flex;
+  position: fixed;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px 0;
+  background-color: var(--slack-color);
+  color: white;
+`;
+
+const LeftSide = styled.div`
+  flex: 0.3;
+  align-items: center;
+  display: flex;
+  margin-left: 20px;
+
+  > .MuiSvgIcon-root {
+    margin-left: auto;
+    margin-right: 30px;
+  }
+`;
+
+const HeaderAvatar = styled(Avatar)`
+  cursor: pointer;
+
+  :hover {
+    opacity: 0.8;
+  }
+`;
+
+const Middle = styled.div`
+  flex: 0.4;
+  opacity: 1;
+  border-radius: 6px;
+  background-color: #421f44;
+  text-align: center;
+  display: flex;
+  padding: 0 50px;
+  color: gray;
+  border: 1px gray solid;
+
+  > input {
+    background-color: transparent;
+    border: none;
+    text-align: center;
+    min-width: 30vw;
+    outline: 0;
+    color: white;
+  }
+`;
+
+const RightSide = styled.div`
+  flex: 0.3;
+  display: flex;
+  align-items: flex-end;
+
+  > .MuiSvgIcon-root {
+    margin-left: auto;
+    margin-right: 20px;
   }
 `;
